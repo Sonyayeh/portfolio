29a56c4a1f463c2e0cbcbb139372734095a3a84a
Sonya <cyeh27@my.bcit.ca>
2024-11-18 20:53:57 -0800

    Project box
    created project box from css.

--- src/Components/GraphicDesign.jsx
+++ src/Components/GraphicDesign.jsx
@@ -1,13 +1,66 @@
 import React from "react";
-import graphic from "../assets/graphicbox.png"; // Import the image correctly
-import styles from "../Components/graphic.module.css";
+import BusVolley from "../mockups/busstop.png";
+
+
 
 const GraphicBox = () => {
     return (
-        <div className={`${styles.GraphicDesignBox}`}>
-            <div className="">
-                <img src={graphic} alt="box for graphic design project" /> {/* Use the imported image here */}
+
+        // the project box
+        <div className="w-full lg:max-w-[1315px] relative lg:ml-[8rem] md:ml-[10rem] mt-[30rem]">
+            <div>
+                <h1 className="flex pb-[-1rem]">Graphic Design:</h1>
+                {/* blue headinig */}
+                <div className=" bg-blue-200 px-4 flex justify-between items-center">
+
+                    <h5 className="flex text-white text-3xl sm:text-xl md:text-6xl lg:text-7xl">
+                        Project
+                    </h5>
+                    <h5 className="text-white text-3xl sm:text-xl md:text-6xl lg:text-8xl ">
+                        X
+                    </h5>
+
+                </div>
+                <div>
+                    {/* this is the white background */}
+                    {/* <div className="lg:w-[1240px] bg-white"></div> */}
+                    <div>
+                        {/* this is the white tab bar */}
+                        <div className=" bg-blue-200 p-4 pt-0">
+                            {/* the list words */}
+                            <ul className="flex gap-6 items-center space-x-1 bg-white w-full px-6">
+                                {/* List items with responsive font sizes */}
+                                <li className="text-blue-200 text-lg sm:text-sm md:text-6xl lg:text-7xl font-semibold">DISC</li>
+                                <li className="text-blue-200 text-lg sm:text-sm md:text-6xl lg:text-7xl font-semibold">OPTION</li>
+                                <li className="text-blue-200 text-lg sm:text-sm md:text-6xl lg:text-7xl font-semibold">VIEW</li>
+                                <li className="text-blue-200 text-lg sm:text-sm md:text-6xl lg:text-7xl font-semibold">HELP</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
+
+            {/* border for the box */}
+            <div>
+
+            {/* Project heading here */}
+            <div className="p-4 sm:p-2 md:p-3 flex border-solid border-x-[1rem] border-b-[1rem] border-blue-200">
+                <div className="space-y-4 flex py-10 px-10">
+                    <div className="">
+                        <h5 className="text-3xl sm:text-xl md:text-6xl pb-[10rem]">
+                            Beach Volleyball Tournament Poster
+                        </h5>
+                        <h4 className="text-lg sm:text-sm md:text-3xl leading-3">
+                            The volleyball tournament poster project is to create a promotional poster about a fictional volleyball match in the Town of Shelbyville. All the elements on the poster were to be created only with Adobe Illustrator.
+                        </h4>
+                    </div>
+                    <img src={BusVolley} alt="Busstop Mockup" className=
+                    'max-w-[27rem] h-auto ' />
+                </div>
+            </div>
+            </div>
+
+        
         </div>
     );
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,121 +1,43 @@
 import React from "react";
-import GraphicBox from "../Components/GraphicDesign";
-import UiuxBox from "../Components/uiux";
-import DevelopBox from "../Components/develop";
+
 import Arrow from '../Components/ArrowSvg';
 import Footer from "../Components/footer";
 import Social from "../Components/social";
-import BusVolley from "../mockups/busstop.png";
-import MoreInfoArrow from "../Components/moreArrow";
+import Graphic from "../Components/GraphicDesign";
 
 import styles from "../pages/Projects.module.css";
 
 
 const Projects = () => {
     return (
-        <div className="container mx-auto px-5">
-            <div className="card">
-
-                <div className={`${styles.title}`}>
+        <div >
+            <div  />
+                {/* page title */}
+                <div className='flex justify-center'>
                     <div>
                         <h1>Projects:</h1>
                     </div>
-                </div>
 
                 {/* the down arrow */}
-                <div className={`${styles.projectArrow}`}>
-                    <div>
-                        <Arrow />
+                    <div className={`${styles.arrowWrapper}`}>
+                        <div>
+                            <Arrow />
+                        </div>
                     </div>
                 </div>
 
-                {/* Graphic Design title */}
-                <div className={`${styles.projectHeader}`}>
-                    <div>
-                        <h1>Graphic Design:</h1>
-                    </div>
-                </div>
-
-                {/* Graphic Design container */}
+                {/* graphic design */}
                 <div>
-                    <GraphicBox />
+                    <Graphic />
                 </div>
-                {/* volleyball mockup here */}
+
                 <div>
-                <img src={BusVolley} alt="Busstop Mockup" className={`${styles.image}`} />
-                </div>
-                {/* more info arrow */}
-                <div className={`${styles.graphicArrowWrapper}`}>
-                    <MoreInfoArrow />
-                </div>
-
-                
-
-                {/* UI/UX Design title */}
-                <div className={`${styles.uiuxprojectHeader}`}>
-                    <div>
-                        <h1>UI/UX Design:</h1>
-                    </div>
-                </div>
-
-               {/* UI/UX containers */}
-                <div className={`${styles.uiuxContainer}`}>
-                    <div className={`${styles.uiuxBoxWrapper}`}>
-                        <UiuxBox />
-                    </div>
-                        <div className={`${styles.arrowWrapper}`}>
-                            <MoreInfoArrow />
-                        </div>
-                    <div className={`${styles.uiuxBoxWrapper}`}>
-                        <UiuxBox />
-                    </div>
-                        <div className={`${styles.arrowWrapper}`}>
-                            <MoreInfoArrow />
-                        </div>
-                    <div className={`${styles.uiuxBoxWrapper}`}>
-                        <UiuxBox />
-                    </div>
-                        <div className={`${styles.arrowWrapper}`}>
-                            <MoreInfoArrow />
-                        </div>
-                    <div className={`${styles.uiuxBoxWrapper}`}>
-                        <UiuxBox />
-                    </div>
-                        <div className={`${styles.arrowWrapper}`}>
-                            <MoreInfoArrow />
-                        </div>
-                    </div>
-                    
-                </div>
-
-                
-
-                {/* Development container */}
-                <div>
-                    <DevelopBox />
-                </div>
-                
-                {/* Develop title */}
-                <div className={`${styles.devprojectHeader}`}>
-                    <div>
-                        <h1>Development:</h1>
-                    </div>
-                </div>
-
-                <div className={`${styles.projectContact}`}>
                     <div>
                         <Footer />
-                    </div>
-                    <div  className={`${styles.devArrowWrapper}`}>
-                        <MoreInfoArrow />
-                    </div>
-                </div>
-
-                <div>
-                    <div>
                         <Social />
                     </div>
                 </div>
+
             </div>
 
     );
