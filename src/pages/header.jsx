2497e1ae0138f1a3f6c871dff637dd4f66edd2b7
Sonya <cyeh27@my.bcit.ca>
2024-11-30 18:25:45 -0800

    logo for home page

--- src/pages/header.jsx
+++ src/pages/header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
           className="h-16 w-auto 
           sm:pl-[18rem]
           md:pl-[50rem] md:mt-[-2rem] md:h-[8rem]
-          lg:pl-[80rem] lg:mt-[-5rem] lg:mb-[1rem] lg:h-[10rem]" 
+          lg:pl-[70rem] lg:mt-[-5rem] lg:mb-[1rem] lg:h-[10rem]" 
         />
       </div>
 
