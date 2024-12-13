75d15c2e6219df2303a48fa3aeb03a0c04c1748b
Sonya <cyeh27@my.bcit.ca>
2024-12-13 14:31:48 -0800

    added meta tags

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { scaleRotate as Menu } from "react-burger-menu"; // Import the hamburger menu from react-burger-menu
+import { Helmet } from "react-helmet"; // Import Helmet for meta tags
 import Layout from "./Components/Layout"; // Layout component that wraps around all pages
 
-
-
-// Import  pages
+// Import pages
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -17,7 +16,7 @@ import Boutique from "./pages/BoardWalk";
 import Bcit from "./pages/MyBcit";
 import Northstar from "./pages/NorthOmu";
 
-// Import  styles
+// Import styles
 import "./reset.css";
 import "./App.css";
 import "./Menu.css";
@@ -27,18 +26,18 @@ import './index.css';
 function App() {
   return (
     <BrowserRouter>
-      
       <div id="outer-container" className="flex flex-col min-h-screen">
-        
-        {/* I have this greyed out just in case i need anything from here */}
-        {/* Desktop Navbar - Visible on large screens */}
-        {/* <nav className="hidden lg:flex text-h2 font-Micro text-blue-300 justify-center align-middle py-4">
-          <Link to="/" className="px-10 hover:text-blue-300 hover:underline">Home</Link>
-          <Link to="/projects" className="px-10 hover:text-blue-300 hover:underline">Projects</Link>
-          <Link to="/about" className="px-10 hover:text-blue-300 hover:underline">About</Link>
-          <Link to="/contact" className="px-10 hover:text-blue-300 hover:underline">Contact</Link>
-          <Link to="/cases" className="px-10 hover:text-blue-300 hover:underline">Cases</Link>
-        </nav> */}
+        {/* Helmet for global meta tags, basic ones first, but will add more! */}
+        <Helmet>
+          <title>My Awesome Website</title>
+          <meta name="description" content="This is my portfolio" />
+          <meta name="keywords" content="React, Web Development, Portfolio, Graphic Design, UI/UX Design, Web Design" />
+          <meta name="author" content="Sonya Yeh" />
+          <meta property="og:title" content="Sonya Yeh's Portfolio" />
+          <meta property="og:description" content="Check out my portfolio and contact me for any further questions!" />
+          <meta property="og:url" content="https://sonyayeh.ca/" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </Helmet>
 
         {/* Mobile Burger Menu (Visible on small and medium screens) */}
         <div className="lg:hidden">
@@ -61,15 +60,14 @@ function App() {
               <Route path="/contact" element={<Contact />} />
               <Route path="/projects" element={<Projects />} />
               <Route path="/cases" element={<Cases />} />
-              <Route path="/travelphotofolder" element={<TravelPictures />} /> {/* Add this route */}
-              <Route path="/beachball" element={<Vollyball />} /> {/* Add this route */}
+              <Route path="/travelphotofolder" element={<TravelPictures />} />
+              <Route path="/beachball" element={<Vollyball />} />
               <Route path="/boardwalk" element={<Boutique />} />
               <Route path="/bcit" element={<Bcit />} />
               <Route path="/northstar" element={<Northstar />} />
             </Route>
           </Routes>
         </main>
-
       </div>
     </BrowserRouter>
   );
