4154435b32366d33cb73af0fd9e5c0f364e38b36
Sonya <cyeh27@my.bcit.ca>
2026-07-31 00:08:29 -0700

    scrolling to the top when landed on furrever pals

--- src/pages/PuppyYoga.jsx
+++ src/pages/PuppyYoga.jsx
@@ -14,7 +14,9 @@ const PuppyYoga = () => {
     // State for the active tab
     const [activeTab, setActiveTab] = useState('ABOUT');
 
-    
+    useEffect(() => {
+              window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+            }, []);
 
     // If project not found, return a message
     if (!project) {
