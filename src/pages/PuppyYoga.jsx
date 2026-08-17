0f4a531fe159586b9f155d05a6130c2bc2c0cd9a
Sonya <cyeh27@my.bcit.ca>
2026-08-17 16:34:48 -0700

    testing
    for some reason the mobile screen and my phone screen arent the same

--- src/pages/PuppyYoga.jsx
+++ src/pages/PuppyYoga.jsx
@@ -50,7 +50,7 @@ const PuppyYoga = () => {
                 </h1>
                 <div className="flex justify-self-center text-center">
               <h3 className="text-orange-500 text-left 
-                sm:text-[1rem] sm:pb-[1rem] sm:w-[22rem] 
+                sm:text-[1rem] sm:pb-[1rem] sm:w-[24rem] 
                 md:text-[2rem] md:w-[50rem] md:pb-[2rem]
                 lp:text-[1.5rem] lp:w-[60rem] lp:leading-[3rem] lp:pb-[4rem]
                 lg:text-[3rem] lg:w-[85rem] lg:mb-[5rem] lg:mt-[2rem] lg:leading-[5rem]">
